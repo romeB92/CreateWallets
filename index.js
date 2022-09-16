fc959f1979979378a84ce2fccc7768f711e7c8a6
0xdaebak <abw2912@gmail.com>
2022-09-16 20:11:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const { ethers } = require("ethers");
 //By : 0xdaebak
 //https://github.com/0xdaebak
 
-//Generate Wallet
-let createWallet = () => {
+//Generate Single Wallet
+let singleWallet = () => {
     let createWallet = ethers.Wallet.createRandom();
 
     console.log('address:', createWallet.address)
@@ -12,4 +12,26 @@ let createWallet = () => {
     console.log('privateKey:', createWallet.privateKey)
 
 }
-createWallet();
+
+//Generate Multi Wallet
+let multiWallet = () =>{
+
+    //create 5 wallets
+    let walletCount = 5;
+
+    //wallet Array
+    let wallets = [];
+
+    for (let i = 0; i < walletCount; i++) {
+        let createWallet = ethers.Wallet.createRandom();
+        wallets.push(createWallet);
+    }
+
+    console.log(wallets[0].address);
+    console.log(wallets[1].address);
+    console.log(wallets[2].address);
+    console.log(wallets[3].address);
+    console.log(wallets[4].address);
+
+}
+singleWallet();
